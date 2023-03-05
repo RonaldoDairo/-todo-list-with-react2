f4a4cbd138349ceb43db5cdeac982a73a9f7c5c9
RonaldoDairo <programaciondairo@gmail.com>
2023-03-05 00:40:13 +0000

    agregando todolist hecho con onkeypress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,57 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React,{useState} from "react";
 
 //create your first component
 const Home = () => {
+		const [inputValue, setInputValue] = useState("");
+		const [todos , setTodos] = useState([]);
+		console.log(inputValue)
+		//add into array => concat
+		//Delete from array -> filter
+		// Update -> map
+			const minLength =3
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="titulo">Todos</h1>
+			<div className="conten">
+			<ul className="lista">
+				<li>
+					<input 
+					
+					type="text" 
+					onChange={(e)=> setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyPress={(e)=>{
+					   if(e.key === "Enter"  && inputValue.length >= minLength){
+							setTodos(todos.concat([inputValue]));
+							setInputValue([]);
+							
+					   }
+					}}
+					placeholder="What you needs to be done?"
+									
+					/>
+					
+				</li>
+				{todos.map((item,index)=>(
+				<ul className="printul">	<li className="print">
+					{item}{""}
+						<i
+						class ="fas fa-trash-alt fa"
+						onClick={()=>
+							setTodos(
+								todos.filter (
+									(t,currentIndex) =>
+										index != currentIndex
+								)
+							)
+						}></i>	
+				</li></ul>
+			))}
+			<div className="conteo"> {todos.length}item left </div>
+			</ul>
+			
+			
+			</div>
 		</div>
 	);
 };
